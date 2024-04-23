e77beb9071485e5dfb6528d458b67acc6721d9ed
paulogmedeiros <paulogsantosm@gmail.com>
2024-04-23 10:28:55 -0300

    input tela de cadastro

--- frontend/src/views/Donos_pet/Cadastro_usuario/Cadastro_usuario.jsx
+++ frontend/src/views/Donos_pet/Cadastro_usuario/Cadastro_usuario.jsx
@@ -5,16 +5,21 @@ function Cadastro_usuario() {
   return (
     <div className='container'>
       <div className='imagem'>
-        <img src={cachorro} alt="foto fofa de cachorro" height={600} width={600}/>
+        <img src={cachorro} alt="foto fofa de cachorro" height={600} width={600} />
       </div>
       <div className='formulario'>
-        <h1><b>Boas Vindas!</b></h1>
-        <form action="">
-          <input type="text" name="nome" id="" placeholder="Nome e sobrenome"/>
-          <input type="text" name="email" id="" placeholder="Email"/>
-          <input type="text" name="senha" id="" placeholder="Senha"/>
-          <input type="text" name="confirmarSenha" id="" placeholder="Confirmar Senha"/>
-          <button>Criar conta</button>
+        <div className='titulo'>
+          <h1><b>Boas Vindas!</b></h1>
+        </div>
+        <form action="" id='form' className='form'>
+          <div className='input'>
+            <input type="text" name="nome" id="nome" placeholder="Nome e sobrenome" />
+            <a>aqui vem a mensagem de erro</a>
+          </div>
+          {/* <input type="text" name="email" id="" placeholder="Email" />
+          <input type="text" name="senha" id="" placeholder="Senha" />
+          <input type="text" name="confirmarSenha" id="" placeholder="Confirmar Senha" />
+          <button>Criar conta</button> */}
         </form>
         <p>Já possui uma conta?<a href="">Entrar</a></p>
       </div>
