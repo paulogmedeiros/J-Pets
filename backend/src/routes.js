1b76b812c979f784c2b92c7f287b9fcb3ee789ec
paulogmedeiros <paulogsantosm@gmail.com>
2024-04-27 21:41:34 -0300

    construção dos diretorios de controller service e repository de servico

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const router = express.Router
 const routes = new router()
+const servico = require("./controllers/servicosController")
 
 
 routes.get("/", (req,res)=>{
-    res.json({teste:"teste"})
-})
+    res.json({mensagem_do_dia:"Já sorriu hoje?"})
+});
+
+// rotas de servicos
+routes.get(("/servicos"),servico.getServico);
 
 
 module.exports = routes
