96558be580f8a542b6a782ef511be3b79d034e1a
Jamille Galazi <jamillegalazidf7@gmail.com>
2024-05-11 14:14:11 -0300

    Finalizando tela de login com responsividade aplicada

--- frontend/src/views/Todos/Login/Login.jsx
+++ frontend/src/views/Todos/Login/Login.jsx
@@ -11,30 +11,36 @@ function Login() {
 
         <div className="container">
 
-                <div className="row col-12 ps-4 col-md-10 position-absolute top-50 start-50 translate-middle">
+            <div className="row justify-content-center col-12 ps-4 col-md-8 position-absolute top-50 start-50 translate-middle ">
 
-                    <div className="col-md-5 d-flex-md-5">
-                    <p class="m-4 ps-5 fs-1 fw-bold">Login</p>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label for="floatingInput">Email</label>
-                        </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                            <label for="floatingPassword">Senha</label>
-
-                        </div>
-                        <a class="mt-3 mb-3 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover d-flex" href="#">
-                            Esqueceu a senha?
-                        </a>
-
-                        <a class="btn btn-primary w-100" href="#" role="button">Acessar</a>
+                <div className="col-md-5 d-flex-md-5">
+                    <p class="login_titulo fs-1 fw-bold text-center mb-2 mb-md-5">Login</p>
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                        <label for="floatingInput">Email</label>
                     </div>
+                    <div class="form-floating mt-md-5">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                        <label for="floatingPassword">Senha</label>
 
-                    <div className="img_login col-md-5 d-flex mt-3 mt-md-0 rounded-4">
-                        <img src={imagem_login} class="img-fluid"></img>
                     </div>
+                    <a class="esqueceu_a_senha_login mt-3 mb-3 link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover d-flex" href="#">
+                        Esqueceu a senha?
+                    </a>
+
+                    <a class="btn_login btn w-100 " href="#" role="button">Acessar</a>
+
+                    <p class="text-body-dark text-center mt-4">
+                        Não possui uma conta? <a href="#" class="esqueceu_a_senha_login link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Registre-se</a>
+                    </p>
                 </div>
+
+                <div className="img_login col-md-5 d-flex mt-3 mt-md-0 rounded-4">
+                    <img src={imagem_login} class="img-fluid"></img>
+                </div>
+
+
+            </div>
 
         </div>
 
